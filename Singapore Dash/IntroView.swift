06feb5b416fe03f447fdbcecbcb707a393393d91
Ingo <ingocodingbecauseyes@gmail.com>
2023-07-22 12:02:35 +0800

    navigation again

--- Singapore Dash/IntroView.swift
+++ Singapore Dash/IntroView.swift
@@ -13,16 +13,19 @@ struct IntroView: View {
         NavigationStack{
             VStack{
                 Text("Intro to the game")
-                    .font(.system(size: 36))
+                    .font(.system(size: 46))
+                    .offset(y: -100)
                 Text("Oh no! Thet Pai and Ingo need to go around Singapore to take photos for their Nursery school project due in one second!")
                     .multilineTextAlignment(.center)
                 Text("They need your help to go to famous landmarks of Singapore so that they can get a Fantastic F- on their project to find their father.")
                     .multilineTextAlignment(.center)
                 Text("You have to help them the fatherless children answer questions at the locations so they can redeem their daddy privileges!")
                     .multilineTextAlignment(.center)
-                Text("This is also a speedrun, so its highly encouragedsweat the living hell off so that they can get demerit points!")
+                Text("This is also a speedrun, so its highly encouraged to sweat the living hell off so that they can get demerit points!")
                     .multilineTextAlignment(.center)
-                
+                NavigationLink(destination:OhioView()) {
+                    Text("Lets go!")
+                }
                 
                 
             }
