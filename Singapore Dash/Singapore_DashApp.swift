c1dc24258ebd0fd7d230092249d72c80bd2916a5
PhyoTP <phyo.thet.pai@sji.edu.sg>
2023-07-22 10:58:00 +0800

    Initial Commit

---
+++ Singapore Dash/Singapore_DashApp.swift
@@ -0,0 +1,17 @@
+//
+//  Singapore_DashApp.swift
+//  Singapore Dash
+//
+//  Created by T Krobot on 22/7/23.
+//
+
+import SwiftUI
+
+@main
+struct Singapore_DashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
