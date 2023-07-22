2b2010f0598bf7d5a4bb9845022634eeb3ad84f1
PhyoTP <phyo.thet.pai@sji.edu.sg>
2023-07-22 16:20:52 +0800

    added map

---
+++ Singapore Dash/QuestionView.swift
@@ -0,0 +1,20 @@
+//
+//  QuestionView.swift
+//  Singapore Dash
+//
+//  Created by T Krobot on 22/7/23.
+//
+
+import SwiftUI
+
+struct QuestionView: View {
+    var body: some View {
+        Image("botanic garden")
+    }
+}
+
+struct QuestionView_Previews: PreviewProvider {
+    static var previews: some View {
+        QuestionView()
+    }
+}
