a787c01a6399fae73dc761562b09c31371f56a77
Ingo <ingocodingbecauseyes@gmail.com>
2023-07-25 18:53:11 +0800

    added some questions here and there

--- Singapore Dash/Question.swift
+++ Singapore Dash/Question.swift
@@ -6,5 +6,5 @@ struct question {
     var option2: String
     var option3: String
     var option4: String
-    var answer: String
+    var answer: Int
 }
--- Singapore Dash/QuestionView.swift
+++ Singapore Dash/QuestionView.swift
@@ -1,7 +1,45 @@
 import SwiftUI
 
 struct QuestionView: View {
-    
+    var questions = [
+        question(ask: "Which is the oldest botanic garden in Singapore?",
+                 option1: "Jurong Bird Park",
+                 option2: "Singapore Zoo",
+                 option3: "Gardens by the Bay",
+                 option4: "Singapore Botanic Gardens",
+        answer: 4),
+        question(
+                ask: "What is the famous landmark inside Singapore Botanic Gardens?",
+                option1: "Supertree Grove",
+                option2: "Cloud Forest",
+                option3: "Swan Lake",
+                option4: "National Orchid Garden",
+                answer: 4
+            ),
+        question(
+                ask: "What type of garden is the National Orchid Garden?",
+                option1: "Japanese Garden",
+                option2: "Rose Garden",
+                option3: "Orchid Garden",
+                option4: "Cactus Garden",
+                answer: 3
+            ),
+        question(
+                ask: "Which botanic garden is known for its collection of tropical rainforest trees?",
+                option1: "HortPark",
+                option2: "Chinese Garden",
+                option3: "Botanic Gardens",
+                option4: "Bay East Garden",
+                answer: 3
+            ),
+        question(
+                ask: "What is the main entrance of Singapore Botanic Gardens called?",
+                option1: "Tanglin Gate",
+                option2: "Orchard Gate",
+                option3: "Tyersall Gate",
+                option4: "Bukit Timah Gate",
+                answer: 1)
+    ]
     @State private var isImageVisible = true // Add a @State variable to track visibility
     func button(_ num: Int, colour: Color) -> some View {
         Button {
